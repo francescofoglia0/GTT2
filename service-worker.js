142950fa0d741b896763766d9163442d41b17806
francescofoglia0 <francifoglia182@gmail.com>
2025-04-07 12:55:05 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (event) => {
-    console.log('Service Worker: Installato');
-});
-
-self.addEventListener('fetch', (event) => {
-    event.respondWith(fetch(event.request));
-});
